0904d1bcd0885ab5b155b368eb572f8c027e89c3
qa-tdnin-1-vihw@example.com <qa-tdnin-1-vihw@example.com>
2023-05-02 15:15:47 +0000

    First commit of promoted project

---
+++ .spectral.mjs
@@ -0,0 +1,2 @@
+import ruleset from "https://x-16286.stoplight-dev.com/api/v1/projects/cHJqOjQ4NQ/spectral.js?branch_slug=main&token=0f2294fe-feb1-4cfc-bdbe-b50130bde02d";
+export default { extends: ruleset };
